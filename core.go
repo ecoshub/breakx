2729616e3382871da95b5e1999b67a4a0869b70a
ecoshub <ocakemrecan@gmail.com>
2020-09-17 09:24:40 +0300

    utils->core name change, file.func()->func() return change

--- core.go
+++ core.go
@@ -14,25 +14,22 @@ func getLine() (string, string, string) {
 		lineOffset        int
 		fileOffset        int
 		stackStringLength int
+		index             int
 	)
 	stackString = string(debug.Stack())
 	tokens := strings.Split(stackString, "\n")
-	// cutoff first and last
 	tokens = tokens[1 : len(tokens)-1]
 	funcLine := tokens[stackNumber]
 	rootLine := tokens[stackNumber+1]
 	tokens = strings.Split(rootLine, " ")
-	// this is stack string
 	stackString = tokens[0]
 	stackStringLength = len(stackString)
 	for i := 0; i < stackStringLength; i++ {
 		curr := stackString[stackStringLength-i-1]
-		// 58 is this :
-		if curr == 58 {
+		if curr == ':' {
 			lineOffset = stackStringLength - i - 1
 		}
-		// 47 is this /
-		if curr == 47 {
+		if curr == '/' {
 			fileOffset = stackStringLength - i - 1
 			break
 		}
@@ -40,11 +37,12 @@ func getLine() (string, string, string) {
 	line = stackString[lineOffset+1:]
 	file = stackString[fileOffset+1 : lineOffset]
 	for i := 0; i < len(funcLine); i++ {
-		// this 40 is (
-		if funcLine[i] == 40 {
+		if funcLine[i] == '(' {
 			funcLine = funcLine[:i]
 		}
 	}
 	funcLine += "()"
+	index = strings.Index(funcLine, ".")
+	funcLine = funcLine[index+1:]
 	return funcLine, file, line
 }
